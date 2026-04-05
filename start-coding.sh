e5f668e9e926c4e62935b9cb03a7798b8cfb28b3
krolen <kkulagin@gmail.com>
2026-04-05 12:59:34 -0400

    trying other models

--- start-coding.sh
+++ start-coding.sh
@@ -1,11 +1,13 @@
-export LLAMA_CACHE="unsloth/Qwen3.5-27B-GGUF"
+export LLAMA_CACHE="/home/kot/unsloth"
+
+# stable
 ./build/bin/llama-server \
-    -hf unsloth/Qwen3.5-27B-GGUF:UD-Q4_K_XL \
+    -hf unsloth/Qwen3.5-35B-A3B-GGUF:UD-Q4_K_XL \
     -ngl 99 \
     --no-mmap \
     --port 8888 \
     --host 0.0.0.0 \
-    --ctx-size 200000 \
+    --ctx-size 262144 \
     --reasoning off \
     --kv-unified \
     --cache-type-k q8_0 --cache-type-v q8_0 \
@@ -18,7 +20,43 @@ export LLAMA_CACHE="unsloth/Qwen3.5-27B-GGUF"
     --swa-full \
     --cont-batching \
 
+#./build/bin/llama-server \
+#    -m $LLAMA_CACHE/HauhauCS/Qwen3.5-35B-A3B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf \
+#    --mmproj $LLAMA_CACHE/HauhauCS/mmproj-Qwen3.5-35B-A3B-Uncensored-HauhauCS-Aggressive-f16.gguf \
+#    -ngl 99 \
+#    --no-mmap \
+#    --port 8888 \
+#    --host 0.0.0.0 \
+#    --ctx-size 262144 \
+#    --reasoning off \
+#    --kv-unified \
+#    --cache-type-k q8_0 --cache-type-v q8_0 \
+#    --flash-attn on --fit on \
+#    --alias "mymodel" \
+#    --temp 0.6 --top-p 0.95 --top-k 20 --min-p 0.00 \
+#    --jinja \
+#    --ubatch-size 1024 --batch-size 4096 \
+#    --ctx-checkpoints 0 \
+#    --swa-full \
+#    --cont-batching \
+#
 #    --ctx-size 262144 \
 #    --chat-template chatml \
 #    --chat-template-file qwen3.5_chat_template.jinja
 
+
+#    -hf unsloth/Qwen3.5-27B-GGUF:UD-Q4_K_XL \
+
+#llama-cli -m Qwen3.5-35B-A3B-Uncensored-HauhauCS-Aggressive-Q4_K_M.gguf \
+#  --mmproj mmproj-Qwen3.5-35B-A3B-Uncensored-HauhauCS-Aggressive-f16.gguf \
+#  --jinja -c 131072 -ngl 99
+
+#huggingface-cli download HauhauCS/Qwen3.5-35B-A3B-Uncensored-HauhauCS-Aggressive \
+#  --include "mmproj-Qwen3.5-35B-A3B-Uncensored-HauhauCS-Aggressive-f16.gguf" \
+#  --local-dir ~/unsloth/HauhauCS \
+#  --local-dir-use-symlinks False
+#
+#huggingface-cli download HauhauCS/Qwen3.5-35B-A3B-Uncensored-HauhauCS-Aggressive \
+#  --include "*Q4_K_M.gguf" \
+#  --local-dir ~/unsloth/HauhauCS \
+#  --local-dir-use-symlinks False
